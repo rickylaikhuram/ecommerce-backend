ffbd4d315857eca843e07a115266b1cf8516bead
Ricky Laikhuram <rickylaikhuramofficial@gmail.com>
2025-08-24 22:18:35 +0530

    fix script in package json

--- src/types/admin.product.types.ts
+++ src/types/admin.product.types.ts
@@ -1,3 +1,29 @@
 export interface ImageToDelete {
   imageUrl: string;
 }
+export type UserData = {
+  id: string;
+  name: string | null;
+  email: string;
+  phone: string | null;
+  createdAt: Date;
+  orders: {
+    id: string;
+    totalAmount: number;
+    status: string;
+    createdAt: Date;
+  }[];
+};
+export type CustomerWithOrders = {
+  id: string;
+  name: string | null;
+  email: string;
+  phone: string | null;
+  createdAt: Date;
+  orders: {
+    id: string;
+    totalAmount: number;
+    status: string;
+    createdAt: Date;
+  }[];
+};
