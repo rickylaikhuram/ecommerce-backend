2c23e85c368c109f0590a010453a3581e78460e6
Ricky Laikhuram <rickylaikhuramofficial@gmail.com>
2025-10-04 22:00:14 +0530

    change to cancel even if the order is pending

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      // "http://localhost:5173",
       // "https://projectecommerce.pages.dev",
       "https://cloverarena.com",
       "https://www.cloverarena.com"],
